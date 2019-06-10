14b5132c4c9e562a35208c94ea01d9d2167bb8dc
Hemant Kumar <Hemant.Kumar@accolite.com>
2019-06-11 00:40:03 +0530

    Adding persiting functionality using database

--- backend/bike/src/main/java/com/globomatics/bike/models/Bike.java
+++ backend/bike/src/main/java/com/globomatics/bike/models/Bike.java
@@ -3,13 +3,33 @@ package com.globomatics.bike.models;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class Bike {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	private String name;
 	private String email;
-	private String model;
 	private String phone;
+	private String model;
 	private String serialNumber;
 	private BigDecimal purchasePrice;
+	@JsonFormat(shape=JsonFormat.Shape.STRING,pattern = "MM-dd-yyyy")
 	private Date purchaseDate;
 	private boolean contact;
 	public String getName() {
